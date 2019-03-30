a4779e73bc2e99cae81dffcb32fd8c32281fdbe8
Sean O'Gorman <spj.ogorman@gmail.com>
2019-03-30 22:16:42 +0000

    Combined Thompsons & Shunting, added comments

--- Code/Thompsons.py
+++ Code/Thompsons.py
@@ -1,5 +1,51 @@
 #Sean O'Gorman
 
+def shunt(infix):
+    """The Shunting Yard Algorithm for converting infix regular expressions to postfix"""
+
+    #Dictionary of special characters and values representing precedence
+    specials = {'*': 50, '.': 40, '|': 30}
+
+    #Output String
+    pofix = ""
+
+    #Operator stack
+    stack = ""
+
+    #Loop through the input string one character at a time
+    for c in infix:
+        #If an open bracket, push to the stack
+        if c == '(':
+            stack = stack + c
+
+        #If  closing bracket, pop from the stack, push to 
+        #output until open bracket
+        elif c == ')':
+            while stack[-1] != '(':
+                pofix = pofix + stack[-1]
+                stack = stack[:-1] 
+            stack = stack[:-1]
+
+        #If it's an operator push to stack after popping lower or
+        #equal precedence operators from top of stack into output
+        elif c in specials:
+            while stack and specials.get(c, 0) <= specials.get(stack[-1], 0):
+                pofix = pofix + stack[-1]
+                stack = stack[:-1] 
+            stack = stack + c
+
+        #Regular characters are pushed immediately to the output
+        else:
+            pofix = pofix + c
+
+    #Pop all remaining operators from stack to output
+    while stack:
+        pofix = pofix + stack[-1]
+        stack = stack[:-1] 
+    
+    #Return postfix regex
+    return pofix
+
 class state:
     label = None
     edge1 = None
@@ -14,6 +60,8 @@ class nfa:
         self.accept = accept
 
 def compile(pofix):
+    """Compiles a postfix regular expression into an NFA."""
+
     nfastack = []
 
     for c in pofix:
@@ -86,4 +134,3 @@ def compile(pofix):
     #Stack should only have a single nfa at this point
     return nfastack.pop()
 
-print(compile("ab.cd.|"))
